acd422b04dc829e1b5f0c51d88f1640aa1e658e6
dongnaebi <zhouzixian@qq.com>
2023-11-30 07:43:41 +0800

    feat: detail page UI done

--- components/Detail.vue
+++ components/Detail.vue
@@ -3,28 +3,101 @@ const route = useRoute()
 const { locale } = useI18n()
 const id = route.params.id
 const { data, error } = useFetch(`/api/emoji/${id}`, { query: { locale: locale.value } })
+const platform = [
+  {
+    name: 'Apple',
+    imgPath: 'apple'
+  },
+  {
+    name: 'Google',
+    imgPath: 'google'
+  },
+  {
+    name: 'Facebook',
+    imgPath: 'facebook'
+  },
+  {
+    name: 'X / Twitter',
+    imgPath: 'x'
+  },
+  {
+    name: 'Microsoft',
+    imgPath: 'microsoft'
+  },
+  {
+    name: 'Samsung',
+    imgPath: 'samsung'
+  },
+  {
+    name: 'Whatsapp',
+    imgPath: 'whatsapp'
+  },
+  {
+    name: 'Messenger',
+    imgPath: 'messenger'
+  }
+]
 </script>
 
 <template>
-  <div v-if="error" class="text-rose-500 card p-6 mb-6 rounded-2xl">
-    {{ error }}
-    <button class="border border-rose-500 px-2 rounded-full" onclick="window.location.reload()">{{ $t('refresh') }}</button>
-  </div>
-  <div v-if="data">
-    <div class="flex justify-center items-center">{{ data.e }}</div>
-    <div>
-      <h2>{{ data.n }}</h2>
-      <div>
-        Search keyword:
-        <span v-for="k in data.keywords" :key="k">{{ k }}</span>
+  <div class="max-w-[680px] mx-auto my-8 px-4">
+    <div v-if="error" class="text-rose-500 card p-6 mb-6 rounded-2xl">
+      {{ error }}
+      <button class="border border-rose-500 px-2 rounded-full" onclick="window.location.reload()">{{ $t('refresh') }}</button>
+    </div>
+    <div v-if="data">
+      <div class="flex justify-center items-center mx-auto">
+        <span class="text-[128px]">{{ data.e }}</span>
       </div>
-      <div>Unicode version:{{ data.v }}</div>
-      <div>Unicode code: U+{{ data.c.replace(/ /g, ' U+') }}</div>
-      <div>Group: {{ data.g }} > {{ data.s }}</div>
-      <button>Copy</button>
+      <div>
+        <h2 class="font-bold text-2xl color-title text-center">{{ data.n }}</h2>
+        <button class="px-6 h-12 rounded-2xl bg-rose-500 text-white flex items-center mx-auto my-6">
+          <i
+            class="text-xl mr-2"
+            :class="false ? 'icon-[material-symbols--check-circle] text-success' : 'icon-[material-symbols--content-copy-outline]'"
+            aria-hidden="true"
+            role="img"
+          />
+          {{ $t('copyBtn') }}
+        </button>
+        <div class="flex justify-between border-t border-b border-main py-2">
+          <span class="shrink-0 mr-4">{{ $t('unicodeName') }}</span>{{ data.n }}
+        </div>
+        <div class="flex justify-between border-b border-main py-2">
+          <span class="shrink-0 mr-4 my-0.5">{{ $t('searchKeyword') }}</span>
+          <div class="flex items-center flex-wrap justify-end">
+            <span v-for="k in data.keywords" :key="k" class="bg-rose-100 text-rose-700 border border-rose-300 rounded-xl px-2 ml-1 my-0.5">{{ k }}</span>
+          </div>
+        </div>
+        <div class="flex justify-between border-b border-main py-2">
+          <span class="shrink-0 mr-4">{{ $t('version') }}</span>{{ data.v }}
+        </div>
+        <div class="flex justify-between border-b border-main py-2">
+          <span class="shrink-0 mr-4">{{ $t('code') }}</span> U+{{ data.c.replace(/ /g, ' U+') }}
+        </div>
+        <div class="flex justify-between border-b border-main py-2">
+          <span class="shrink-0 mr-4">{{ $t('inGroup') }}</span> <span class="text-right">{{ data.g }} > {{ data.s }}</span>
+        </div>
+      </div>
+      <div class="mt-6">
+        <h3 class="font-bold color-action">{{ $t('otherPlatform') }}</h3>
+        <div
+          class="mt-6 flex justify-between md:block bg-white/90 dark:bg-zinc-800/90 border border-zinc-200/80 dark:border-zinc-700/80 p-4 md:p-0 md:bg-transparent md:dark:bg-transparent md:border-none rounded-2xl"
+        >
+          <div class="md:flex md:border-t md:border-b md:border-main md:py-2">
+            <div v-for="p in platform" :key="p.imgPath" class="md:flex-1 md:text-center h-[72px] leading-[72px] md:h-auto md:leading-normal mb-2 md:mb-0">
+              {{ p.name }}
+            </div>
+          </div>
+          <div class="md:flex md:mt-2">
+            <div v-for="p in platform" :key="p.imgPath" class="mb-2 md:flex-1 md:flex md:justify-center md:tooltip" :data-tip="$t('imgCopyTip')">
+              <img src="/logo.png" width="72" height="72" :alt="$t('platformImg', { name: data.n, platform: p.name })">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-  <div>Show in other platform: Apple:</div>
 </template>
 
 <style scoped></style>
