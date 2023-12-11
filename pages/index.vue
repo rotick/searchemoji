a4fe24458ea7b1ad75239c5ecdad76f0db3e3db8
東奈比 <zhouzixian@qq.com>
2023-12-11 13:58:35 +0800

    fix: add important to dvh

--- pages/index.vue
+++ pages/index.vue
@@ -620,7 +620,7 @@ function modalClick (ev: any) {
   </div>
   <transition name="nested" :duration="150">
     <div v-if="showDetail" class="fixed z-20 top-0 left-0 w-full h-full bg-black/50 dark:bg-black/80 backdrop-blur-sm flex" @click="modalClick">
-      <div ref="iconOverlay" class="inner bg-body h-[90vh] h-[90dvh] w-[100vw] md:w-[760px] rounded-t-3xl shadow-2xl absolute bottom-0 left-0 md:left-1/2 md:-ml-[380px]">
+      <div ref="iconOverlay" class="inner bg-body h-[90vh] !h-[90dvh] w-[100vw] md:w-[760px] rounded-t-3xl shadow-2xl absolute bottom-0 left-0 md:left-1/2 md:-ml-[380px]">
         <a
           href="javascript:;"
           class="absolute z-20 card top-4 right-4 md:top-6 md:right-6 w-8 h-8 rounded-xl text-2xl flex justify-center items-center hover:bg-rose-500 hover:border-rose-500 hover:text-white"
