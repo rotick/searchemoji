58083f2a0f6a0764d5a799cf8bf841383607ea8a
dongnaebi <zhouzixian@qq.com>
2023-12-13 23:12:22 +0800

    test: cloudflare cache test

--- server/plugins/handleQuery.ts
+++ server/plugins/handleQuery.ts
@@ -1,6 +1,6 @@
 export default defineNitroPlugin(nitroApp => {
   nitroApp.hooks.hook('render:response', (response, { event }) => {
-    if (response.headers?.location.includes('?cfcache=')) {
+    if (response.headers?.location?.includes('?cfcache=')) {
       response.headers.location = response.headers.location.replace(/\?[^]*/g, '')
     }
   })
