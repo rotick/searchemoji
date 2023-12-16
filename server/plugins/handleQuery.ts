fd26390eee953a1475b5f9d70d3f8337e5623342
dongnaebi <zhouzixian@qq.com>
2023-12-16 12:54:55 +0800

    feat: no need to replace

--- server/plugins/handleQuery.ts
+++ server/plugins/handleQuery.ts
@@ -5,7 +5,7 @@ export default defineNitroPlugin(nitroApp => {
       const url = new URL(response.headers.location, host)
       const params = new URLSearchParams(url.search)
       params.delete('cfcache')
-      url.search = params.toString().replace(/&$/, '')
+      url.search = params.toString()
       response.headers.location = url.toString().replace(host, '')
       response.headers['Cache-Control'] = 'max-age=86400, must-revalidate'
     }
